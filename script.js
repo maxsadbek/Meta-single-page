3110a3669e5e7e1f51e51de80d1f98ae2efe68e0
maxsadbek <masadanb@gmail.com>
2025-10-21 14:43:47 +0500

    add

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector("#menu");
 const menuBar = document.querySelector(".menu-bar");
+const foundders = document.querySelector("#foundders")
+const partners = document.querySelector("#partners")
+const future = document.querySelector("#future")
 
 menuBtn.addEventListener("click", () => {
     menuBar.classList.toggle("active");
@@ -13,4 +16,29 @@ menuBtn.addEventListener("click", () => {
     }
 });
 
-console.log(menuBtn);
+foundders.addEventListener("click", ()=>{
+    foundders.style.backgroundColor = "#222"
+    foundders.style.color = "rgb(141,253,27)"
+    partners.style.backgroundColor = "transparent"
+    future.style.backgroundColor = "transparent"
+    partners.style.color = "#fff"
+    future.style.color = "#fff"
+})
+
+partners.addEventListener("click", () => {
+    partners.style.backgroundColor = "#222"
+    partners.style.color = "rgb(141,253,27)"
+    foundders.style.backgroundColor = "transparent"
+    future.style.backgroundColor = "transparent"
+    foundders.style.color = "#fff"
+    future.style.color = "#fff"
+})
+
+future.addEventListener("click", () => {
+    future.style.backgroundColor = "#222"
+    future.style.color = "rgb(141,253,27)"
+    foundders.style.backgroundColor = "transparent"
+    partners.style.backgroundColor = "transparent"
+    foundders.style.color = "#fff"
+    partners.style.color = "#fff"
+})
